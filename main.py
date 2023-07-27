c4c3d8aec8eb3c17672474eebe081cdc7a24c1f7
Satya Patel <satyapatel2004@icloud.com>
2023-07-27 14:28:58 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split 
+from sklearn.tree import DecisionTreeRegressor 
+
+print("hello world") 
